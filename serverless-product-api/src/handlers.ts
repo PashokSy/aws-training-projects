3506c4d890eb3193cc103b411e1118067732f456
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-06 20:32:30 +0300

    feat: updateProduct, deleteProduct, listProducts

--- serverless-product-api/src/handlers.ts
+++ serverless-product-api/src/handlers.ts
@@ -1,52 +1,179 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import AWS from "aws-sdk";
 import { v4 } from "uuid";
+import * as yup from "yup";
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 const tableName = "ProductsTable";
+const headers = {
+  "content-type": "application/json",
+};
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  description: yup.string().required(),
+  price: yup.number().required(),
+  available: yup.bool().required(),
+});
 
 export const createProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const reqBody = JSON.parse(event.body as string);
+  try {
+    const reqBody = JSON.parse(event.body as string);
 
-  const product = {
-    ...reqBody,
-    productID: v4(),
-  };
+    await schema.validate(reqBody, { abortEarly: false });
 
-  await docClient
-    .put({
-      TableName: tableName,
-      Item: product,
-    })
-    .promise();
+    const product = {
+      ...reqBody,
+      productID: v4(),
+    };
 
-  return {
-    statusCode: 201,
-    body: JSON.stringify(product),
-  };
+    await docClient
+      .put({
+        TableName: tableName,
+        Item: product,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (error) {
+    return handleError(error);
+  }
 };
 
 export const getProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const id = event.pathParameters?.id;
+  try {
+    const id = event.pathParameters?.id;
+    const product = await fetchProductById(id as string);
 
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const updateProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const id = event.pathParameters?.id;
+
+    await fetchProductById(id as string);
+
+    const reqBody = JSON.parse(event.body as string);
+
+    await schema.validate(reqBody, { abortEarly: false });
+
+    const product = {
+      ...reqBody,
+      productID: id,
+    };
+
+    await docClient
+      .put({
+        TableName: tableName,
+        Item: product,
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const deleteProduct = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const id = event.pathParameters?.id;
+    await fetchProductById(id as string);
+
+    await docClient
+      .delete({
+        TableName: tableName,
+        Key: { productID: id },
+      })
+      .promise();
+
+    return {
+      statusCode: 204,
+      headers,
+      body: "",
+    };
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
+export const listProducts = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const output = await docClient.scan({ TableName: tableName }).promise();
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify(output.Items),
+  };
+};
+
+async function fetchProductById(id: string) {
   const output = await docClient
     .get({
       TableName: tableName,
-      Key: {
-        productID: id,
-      },
+      Key: { productID: id },
     })
     .promise();
 
   if (!output.Item) {
+    throw new HttpError(404, { error: "not found" });
+  }
+
+  return output.Item;
+}
+
+class HttpError extends Error {
+  constructor(public statusCode: number, body: Record<string, unknown> = {}) {
+    super(JSON.stringify(body));
+  }
+}
+
+function handleError(error: unknown) {
+  if (error instanceof yup.ValidationError) {
     return {
-      statusCode: 404,
-      body: JSON.stringify({ error: "not found" }),
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        errors: error.errors,
+      }),
     };
   }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(output.Item),
-  };
-};
+  if (error instanceof SyntaxError) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        error: `invalid request body format: "${error.message}"`,
+      }),
+    };
+  }
+
+  if (error instanceof HttpError) {
+    return {
+      statusCode: error.statusCode,
+      headers,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+    };
+  }
+
+  throw error;
+}
