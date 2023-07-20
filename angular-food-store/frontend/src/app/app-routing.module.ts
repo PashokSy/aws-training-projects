12143737f217cf75801f53bbfae570ffef065982
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-20 18:26:43 +0300

    feat: Tags

--- angular-food-store/frontend/src/app/app-routing.module.ts
+++ angular-food-store/frontend/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search/:searchTerm', component: HomeComponent },
   { path: 'food/:id', component: FoodPageComponent },
+  { path: 'tag/:tag', component: HomeComponent },
 ];
 
 @NgModule({
--- angular-food-store/frontend/src/app/app.module.ts
+++ angular-food-store/frontend/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { SearchComponent } from './components/partials/search/search.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, FoodPageComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, FoodPageComponent, TagsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
