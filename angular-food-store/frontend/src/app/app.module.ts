82a62889ae36ba5b5a22de1ded7550807f73cb03
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-20 19:51:51 +0300

    feat: checkout

--- angular-food-store/frontend/src/app/app.module.ts
+++ angular-food-store/frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SearchComponent } from './components/partials/search/search.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
     FoodPageComponent,
     TagsComponent,
     CartPageComponent,
+    TitleComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
