bd322579efc13bee5448c0981ea73a72ee5f815f
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-07-22 21:30:26 +0300

    feat: front connection to back

--- angular-food-store/frontend/src/app/components/pages/home/home.component.ts
+++ angular-food-store/frontend/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -10,21 +11,23 @@ import { Food } from 'src/app/shared/models/Food';
 })
 export class HomeComponent implements OnInit {
   foods: Food[] = [];
-
   constructor(
     private foodService: FoodService,
     activatedRoute: ActivatedRoute
   ) {
+    let foodsObservable: Observable<Food[]>;
     activatedRoute.params.subscribe((params) => {
-      if (params['searchTerm']) {
-        this.foods = this.foodService.getAllFoodsBySearchTerm(
-          params['searchTerm']
+      if (params.searchTerm)
+        foodsObservable = this.foodService.getAllFoodsBySearchTerm(
+          params.searchTerm
         );
-      } else if (params['tag']) {
-        this.foods = this.foodService.getAllFoodByTag(params['tag']);
-      } else {
-        this.foods = foodService.getAll();
-      }
+      else if (params.tag)
+        foodsObservable = this.foodService.getAllFoodsByTag(params.tag);
+      else foodsObservable = foodService.getAll();
+
+      foodsObservable.subscribe((serverFoods) => {
+        this.foods = serverFoods;
+      });
     });
   }
 
