5faa9d784e357342b4db969719c47eb67e72409d
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-04-04 16:20:06 +0300

    Electron js Tutorial - 1 - Introduction Electron js Tutorial - 2 - Hello World Electron js Tutorial - 3 - Main and Renderer Process

---
+++ main-render/README.md
@@ -0,0 +1,3 @@
+# Electron js Tutorials
+
+This project build during this [tutorial](https://www.youtube.com/playlist?list=PLC3y8-rFHvwiCJD3WrAFUrIMkGVDE0uqW)
---
+++ main-render/main.js
@@ -0,0 +1,52 @@
+import electron from 'electron';
+import path from 'path';
+import url from 'url';
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let winOne, winTwo
+
+function createWindow() {
+  winOne = new BrowserWindow();
+  winTwo = new BrowserWindow();
+  
+  winOne.loadURL(
+    url.format({
+      pathname: path.join('.', 'one.html'),
+      protocol: 'file',
+      slashes: true,
+    })
+  );
+
+  winTwo.loadURL(
+    url.format({
+      pathname: path.join('.', 'two.html'),
+      protocol: 'file',
+      slashes: true,
+    })
+  );
+
+  winOne.webContents.openDevTools()
+  winTwo.webContents.openDevTools()
+
+  winOne.on('closed', () => {
+    winOne = null;
+  });
+  winTwo.on('closed', () => {
+    winTwo = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('windows-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (winOne === null && winTwo === null) {
+    createWindow();
+  }
+});
